6150984816689e934bb55271119f387f90aef987
Danny North <lds.north@gmail.com>
2017-07-27 14:43:22 -0600

    Add Gesture Controller with dough detection

---
+++ Assets/Scripts/GestureController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using Edwon.VR.Gesture;
+using Edwon.VR;
+
+public class GestureController : MonoBehaviour
+{
+    public GameObject dough;
+    void OnEnable()
+    {
+        GestureRecognizer.GestureDetectedEvent += OnGestureDetected;
+        GestureRecognizer.GestureRejectedEvent += OnGestureRejected;
+    }
+
+    private void OnGestureDetected(string gestureName, double confidence, Handedness hand, bool isDouble = false)
+    {
+        switch(gestureName)
+        {
+            case "Dough Top Left":
+            case "Dough Top Right":
+            case "Dough Bottom Left":
+            case "Dough Bottom Right":
+                Instantiate(dough, transform.position, transform.rotation);
+                Debug.Log("Building Dough");
+                break;
+            
+            default:
+                break;
+        }
+    }
+
+    private void OnGestureRejected(string error, string gestureName = null, double confidence = 0)
+    {
+        
+    }
+}
