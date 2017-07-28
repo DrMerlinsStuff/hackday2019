c513603abab7940b1876ea7f8c9d75dfb7ec95d3
Danny North <lds.north@gmail.com>
2017-07-27 20:15:12 -0600

    Create GameController and static SceneObjects classes

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class GameController : MonoBehaviour
+{
+    private GameObject leftControllerEvents;
+    // Use this for initialization
+
+    void Awake()
+    {
+        VRTK_SDKManager.instance.AddBehaviourToToggleOnLoadedSetupChange(this);
+    }
+
+    void OnDestroy()
+    {
+        VRTK_SDKManager.instance.RemoveBehaviourToToggleOnLoadedSetupChange(this);
+    }
+
+    void OnEnable()
+    {
+        SceneObjects.GestureRig.enabled = false;
+    }
+
+    //Need to figure out how to OnDisable to unsubscribe from events
+
+    void Update()
+    {
+    }
+
+
+    //TODO - check when touching or holding on to gameObject. Disable if so. 
+}
---
+++ Assets/Scripts/SceneObjects.cs
@@ -0,0 +1,83 @@
+﻿using Edwon.VR;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public static class SceneObjects
+{
+    private static VRGestureRig gestureRig;
+    private static GameObject leftController;
+    private static GameObject rightController;
+    private static VRTK_ControllerEvents leftControllerEvents;
+    private static VRTK_ControllerEvents rightControllerEvents;
+
+
+    public static VRGestureRig GestureRig
+    {
+        get
+        {
+            if (gestureRig == null)
+            {
+                //Dont know of a better way than the .Find()  :( 
+                gestureRig = GameObject.Find("[CameraRig]").GetComponent<VRGestureRig>();
+            }
+
+            return gestureRig;
+        }
+    }
+
+    public static GameObject LeftController
+    {
+        get
+        {
+            if(leftController == null)
+            {
+                leftController = VRTK_DeviceFinder.GetControllerLeftHand();
+            }
+
+            return leftController;
+        }
+    }
+
+    //In order for this to work, you have to subscribe to the VRTK_SDKManager on Awake in the class (see GameController.cs for example)
+    public static VRTK_ControllerEvents LeftControllerEvents
+    {
+        get
+        {
+            if(leftControllerEvents == null)
+            {
+                Debug.Log("Making it");
+                leftControllerEvents = LeftController.GetComponent<VRTK_ControllerEvents>();
+            }
+            Debug.Log("just doing whatever");
+            return leftControllerEvents;
+        }
+    }
+
+    public static GameObject RightController
+    {
+        get
+        {
+            if (rightController == null)
+            {
+                rightController = VRTK_DeviceFinder.GetControllerRightHand();
+            }
+
+            return rightController;
+        }
+    }
+
+    public static VRTK_ControllerEvents RightControllerEvents
+    {
+        get
+        {
+            if (rightControllerEvents == null)
+            {
+                rightControllerEvents = RightController.GetComponent<VRTK_ControllerEvents>();
+            }
+
+            return rightControllerEvents;
+        }
+    }
+}
