4fe80e8ca39d28886a2dc64e67a8b9c9e82210fe
Jake Apple <jake-apple@pluralsight.com>
2018-10-25 14:54:14 -0600

    added some crank stuffs

---
+++ Assets/Scripts/CrankValue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class CrankValue : MonoBehaviour
+{
+
+    public VRTK_Knob knob;
+    public GameObject jack;
+    public Vector3 growSize;
+    float currentValue = 0;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    knob.ValueChanged += delegate { OnValueChanged(); };
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+    public void OnValueChanged()
+    {
+        var maxValue = 1000;
+
+        if (currentValue / maxValue < 1 && currentValue++ < maxValue)
+        {
+            jack.transform.localScale += growSize;
+        }
+        else
+        {
+            return;
+        }
+
+
+        //Debug.unityLogger.Log(knob.GetValue());
+        Debug.unityLogger.Log(currentValue / maxValue);
+        //Debug.unityLogger.Log(jack.transform.localScale);
+    }
+}
